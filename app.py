d43458be8e6a9dc5ae6f6c358048a2b1fa3dce08
Smeueg <Smeueg@gmail.com>
2025-10-05 21:32:24 +0700

    Renamed 2021 csv files

--- app.py
+++ app.py
@@ -19,17 +19,23 @@ chosen_category: str = st.selectbox(
     next(os.walk(os.path.join(cwd, "data", chosen_year)))[1]
 )
 
-chosen_set: str = st.selectbox(
-    "Choose the question set",
-    ("challenge", "warmup", "practice")
+chosen_language: str = st.selectbox(
+    "Choose the language",
+    next(os.walk(os.path.join(cwd, "data", chosen_year, chosen_category)))[1]
 )
 
-file_suffix = os.path.join(cwd, "data", chosen_year, chosen_category)
-en_file = os.path.join(file_suffix, "en", chosen_set) + ".csv"
-id_file = os.path.join(file_suffix, "id", chosen_set) + ".csv"
-en_df = pd.read_csv(en_file)
-id_df = pd.read_csv(id_file)
-df = pd.concat([en_df, id_df], ignore_index=True)
+chosen_set: str = st.selectbox(
+    "Choose the question set",
+    next(os.walk(os.path.join(cwd, "data", chosen_year, chosen_category, chosen_language)))[2]
+)
+
+# file_suffix = os.path.join(cwd, "data", chosen_year, chosen_category)
+# en_file = os.path.join(file_suffix, "en", chosen_set) + ".csv"
+# id_file = os.path.join(file_suffix, "id", chosen_set) + ".csv"
+# en_df = pd.read_csv(en_file)
+# id_df = pd.read_csv(id_file)
+# df = pd.concat([en_df, id_df], ignore_index=True)
+df: pd.DataFrame = pd.read_csv(os.path.join(cwd, "data", chosen_year, chosen_category, chosen_language, chosen_set))
 
 correlation_df = df.iloc[:, 6:].replace("-", "0").apply(pd.to_numeric, errors='coerce').corr(method="pearson")
 
