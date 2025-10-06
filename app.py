b726d231b25b71fda1c356161ecb9cd5ee4c96c7
NFYouVel <marvelnathanael072006@gmail.com>
2025-10-06 18:35:21 +0700

    ni brok beres

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ cwd = os.getcwd()
 
 chosen_year: str = st.selectbox(
     "Choose the year",
-    next(os.walk(os.path.join(cwd, "data")))[1]
+    next(os.walk(os.path.join(cwd, "data")))[1],
+    key="selectbox_1"
 )
 
 chosen_category: str = st.selectbox(
@@ -43,3 +44,71 @@ fig, ax = plt.subplots(figsize=(12, 8))
 sns.heatmap(correlation_df, annot=True, cmap='coolwarm', center=0, ax=ax);
 ax.set_title("Pearson Correlation Heatmap")
 st.pyplot(fig)
+
+
+st.divider()
+st.subheader("Perbandingan Nilai Rata-rata per Pertanyaan dan Bahasa")
+chosen_year2: str = st.selectbox(
+    "Choose the year",
+    next(os.walk(os.path.join(cwd, "data")))[1],
+    key="selectbox_2"
+)
+chosen_category2: str = st.selectbox(
+    "Choose the category",
+    next(os.walk(os.path.join(cwd, "data", chosen_year2)))[1],
+    key="selectbox_3"
+)
+folder_utama=os.path.join(cwd, "data", chosen_year2, chosen_category2)
+records=[]
+
+for languange in os.listdir(folder_utama):
+    folder_bahasa=os.path.join(folder_utama, languange)
+    for file_name in os.listdir(folder_bahasa):
+        if chosen_year2=="2020":
+            if file_name=="challenge.csv" :
+                file_path=os.path.join(folder_bahasa, file_name)
+                df= pd.read_csv(file_path)
+                question_cols=df.columns[6:]
+                for i, col in enumerate(question_cols, start=1):
+                    df[col]=pd.to_numeric(df[col], errors="coerce")
+                    mean_grade=df[col].mean(skipna=True)
+                    records.append({
+                        "Category": chosen_category2,
+                        "Languange": languange,
+                        "Question": f"Q{i}",
+                        "Average_grade": mean_grade
+                    })
+        else:
+            if file_name=="grades.csv" :
+                file_path=os.path.join(folder_bahasa, file_name)
+                df= pd.read_csv(file_path)
+                question_cols=df.columns[6:]
+                for i, col in enumerate(question_cols, start=1):
+                    df[col]=pd.to_numeric(df[col], errors="coerce")
+                    mean_grade=df[col].mean(skipna=True)
+                    records.append({
+                        "Category": chosen_category2,
+                        "Languange": languange,
+                        "Question": f"Q{i}",
+                        "Average_grade": mean_grade
+                    })
+#buat data_frame
+summary_df=pd.DataFrame(records)
+st.dataframe(summary_df)
+#visualisasi 
+fig, ax= plt.subplots(figsize=(8,5))
+sns.barplot(
+    data=summary_df,
+    x="Question",
+    y="Average_grade",
+    hue="Languange",
+    palette="Set2",
+    ax=ax,
+    errorbar=None
+)
+ax.set_title("Rata-rata nilai per soal ({chosen_category2}), {chosen_year2}")
+ax.set_xlabel("Soal")
+ax.set_ylabel("Rata-rata nilai")
+ax.legend(title="Bahasa")
+st.pyplot(fig)
+
