525785efb338c7175652a7c4b3808a4807df562d
NFYouVel <marvelnathanael072006@gmail.com>
2025-10-06 21:40:49 +0700

    menambahkan t_test average

--- app.py
+++ app.py
@@ -40,11 +40,13 @@ with tab_correlation:
     fig, ax = plt.subplots(figsize=(12, 8))
     sns.heatmap(correlation_df, annot=True, cmap='coolwarm', center=0, ax=ax);
     ax.set_title(f"Pearson Correlation Heatmap ({chosen_category}, {chosen_year}, {chosen_language})")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=25, ha='center')
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, ha='right')
     st.pyplot(fig)
 
 
 with tab_average:
-    _ = st.subheader("Perbandingan Nilai Rata-rata per Pertanyaan dan Bahasa")
+    _ = st.subheader("Mean differences")
 
     chosen_year: str = st.selectbox(
         "Choose the year",
@@ -109,3 +111,21 @@ with tab_average:
     _ = ax.set_ylabel("Rata-rata nilai")
     _ = ax.legend(title="Bahasa")
     _ = st.pyplot(fig)
+    st.divider()
+    from scipy.stats import ttest_ind
+    st.subheader("Uji Signifikansi (Independent t-test)")
+    en_scores=en_df.iloc[:, 6:].replace("-", "0").apply(pd.to_numeric, errors='coerce')
+    id_scores=id_df.iloc[:, 6:].replace("-", "0").apply(pd.to_numeric, errors='coerce')
+    t_test_result=[]
+    for col in en_scores.columns:
+        en_value=en_scores[col]
+        id_value=id_scores[col]
+        _, p_val = ttest_ind(en_value, id_value, equal_var=False)
+        t_test_result.append({
+            "Question":col,
+            "Significant": "Yes" if p_val <0.05 else "No"
+        })
+    t_df=pd.DataFrame(t_test_result)
+    t_df.set_index(t_df.columns[0], inplace=True)
+    st.dataframe(t_df.T.style.hide(axis="index"), use_container_width=False)
+    # st.dataframe(t_df)
